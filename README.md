c41f700eb409f6f978bd652d1551b97f968663bf
Ludovic L'Hours <ludovic.lhours@zenika.com>
2025-09-29 14:01:52 +0200

    Example of Blender serialization

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # Links
 
 * [Simple Tutorial](https://www.baeldung.com/java-annotation-processing-builder) of Annotation Processor
+* [Blender Serialization](https://developer.blender.org/docs/features/core/dna/)
---
+++ demo/src/main/java/org/github/madbrain/demo/blender/Camera.java
@@ -0,0 +1,11 @@
+package org.github.madbrain.demo.blender;
+
+/**
+ * Collected by <a href="https://github.com/blender/blender/blob/main/source/blender/makesdna/intern/makesdna.cc#L1594">makesdna</a>
+ * tool to build a compact description of all structures
+ * to be embedded in a .blend file.
+ * Allows forward and backward compatibility
+ */
+@DNA
+public class Camera {
+}
